f7d5c4146b794627c41a44dac1bce1b8367c7e14
M-Luq <mohamedluqman766@gmail.com>
2025-02-07 09:28:44 +0530

    AccountService First Commit

---
+++ AccountService/src/main/java/com/example/demo/entity/TransactionDTO.java
@@ -0,0 +1,19 @@
+package com.example.demo.entity;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionDTO {
+//	private int transactionId;
+	private String transactionType;
+	private LocalDate timeOfTransaction;
+	private double updatedBalance;
+	private int accNo;
+
+}
